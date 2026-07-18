66087cbe1c19b133a1db00cedcbc65fa1f006b4b
revoboutique06-prog <revoboutique06@gmail.com>
2026-07-18 14:30:48 +0300

    Update videos.controller.ts

--- videos.controller.ts
+++ videos.controller.ts
@@ -1,3 +1,5 @@
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
 import { Controller, Post, Body } from '@nestjs/common';
 import { VideosService } from './videos.service';
 
@@ -5,6 +7,7 @@ import { VideosService } from './videos.service';
 export class VideosController {
   constructor(private readonly videosService: VideosService) {}
 
+  @UseGuards(AuthGuard)
   @Post('generate')
   async generateVideo(@Body('prompt') prompt: string) {
     return this.videosService.generate(prompt);
